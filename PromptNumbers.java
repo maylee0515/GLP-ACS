50d8b0987a7a7c35fcd159b4c9ff0c107b6bc0fe
Won Jung Lee <maylee0515@gmail.com>
2019-03-06 11:33:37 +0900

    May Lee

--- PromptNumbers.java
+++ PromptNumbers.java
@@ -1,24 +1,19 @@
-/*
+package MiniLab_1;/*
 GLP ACS Mini-Lab 1
-
 This program prompts the user to enter several numbers,
 stores the numbers into an array,
 then prints the numbers in forwards and backwards order.
-
 Expected example output:
-
 How many numbers will you enter? 4
 Type a number: 12
 Type a number: 8
 Type a number: -2
 Type a number: 39
-
 Your numbers in forward order:
 12
 8
 -2
 39
-
 Your numbers in backward order:
 39
 -2
@@ -26,31 +21,47 @@ Your numbers in backward order:
 12
 */
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class PromptNumbers {
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
 		System.out.print("How many numbers will you enter? ");
 		int count = console.nextInt();
+		int[] listo = new int[count];
 		
 		
-		for (int i = ???; i < ???; i++) {
+		for (int i = 0; i < count; i++) {
 			// your code goes here; store the numbers
 			System.out.print("Type a number: ");
+			int enter = console.nextInt();
+			listo[i] = enter;
 			
 		}
 		
 		System.out.println();
 		System.out.println("Your numbers in forward order:");
 		
-		for (int i = ???; i < ???; i++) {
+		for (int i = 0; i < count; i++) {
 			// your code goes here; print the numbers in forward order
+
 		
 		}
+		System.out.println(Arrays.toString(listo));
 
 		System.out.println();
 		System.out.println("Your numbers in backward order:");
-		
+		int[] reverse = new int[count];
 		// your code goes here; print the numbers in backward order
+		for (int i = 0; i < count; i++) {
+			// your code goes here; print the numbers in forward order
+			reverse[i] = listo[count - (i+1)];
+
+
+		}
+
+		System.out.println(Arrays.toString(reverse));
 		
 	}
 }
